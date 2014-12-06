89d792658cec117dd39a2fd6aa73e7bd43722b0d
omniaura <omniaura5@gmail.com>
2014-12-06 13:51:19 -0800

    basic view and template setup

--- generator/views.py
+++ generator/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+
+class FrontView(TemplateView):
+    template_name = 'front.html'
